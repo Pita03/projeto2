31457e52c5a7c8f1f8806d3b76a6375f6204a3ea
Pita03 <joaovsp2@al.insper.edu.br>
2025-10-20 10:39:10 -0300

    exercicio 2

--- funcoes.py
+++ funcoes.py
@@ -13,3 +13,14 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     return posicoes
 
 print('teste')
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+
+    if nome_navio not in frota:
+        frota[nome_navio] = []
+
+    frota[nome_navio].append(posicoes)
+    
+    return frota
