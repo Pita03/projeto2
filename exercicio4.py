bb5bce9264362780b11e9c5e2683b72919384965
CauaOdM <CauaSarraf04@gmail.com>
2025-10-24 00:16:22 -0300

    função de posicionamento de frotas no tabuleiro a partir de um dicionário (exercicio 4)

---
+++ exercicio4.py
@@ -0,0 +1,20 @@
+def posiciona_frota(navio_dicio):
+    tabuleiro_f=[]
+    linha=[]
+
+    for i in range(10):
+        linha = []
+        for c in range(10):
+            linha.append(0)
+        tabuleiro_f.append(linha)
+    
+
+    for posicoes in navio_dicio.values():
+        for coordenadas in posicoes:
+            linha=coordenadas[0]
+            coluna=coordenadas[1]
+            tabuleiro_f[linha][coluna]=1
+    
+    return tabuleiro_f
+
+        
